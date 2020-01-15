751a8688a3e38c4bd86fbb8dce493c38082d832d
Ryan Raettig <raettig@email.arizona.edu>
2020-01-15 14:15:14 -0500

    removed team.lua from task2

--- a1/task2/team.lua
+++
@@ -1,7 +0,0 @@
---      (15) Write a Lua program (filename: team.lua) that prints your team number to the
---      terminal 10 times.
-
---loops through 10 times and prints the team number
-for i=1,10 do 
-    print("Iteration: " .. i .. ", Team Number: 5") 
-end
