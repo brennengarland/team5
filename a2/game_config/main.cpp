07b8ed0ab630450a1f87aec3aec897356ff05fb5
Brennen Linux-VM <bgarland015@gmail.com>
2020-01-24 10:28:28 -0500

    rebasing

--- a2/game_config/main.cpp
+++ a2/game_config/main.cpp
@@ -8,6 +8,7 @@ int main() {
 
    std::cout << "Creating game" << std::endl;
    // read Lua script to determine initial window size infomation
+   local config = require "config";
    auto game = std::make_unique<Game>("1st Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
 
    std::cout << "Starting game loop" << std::endl;
