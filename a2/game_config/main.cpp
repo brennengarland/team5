5c9b671a11dbe36af71ab956e0029599e1874544
Brennen Linux-VM <bgarland015@gmail.com>
2020-01-29 16:56:16 -0500

    starting to make... no window appears

--- a2/game_config/main.cpp
+++ a2/game_config/main.cpp
@@ -1,21 +1,18 @@
 
 #include "Game.hpp"
-
+#include "lua.hpp"
 #include <iostream>
 #include <memory>
-#include <lua.h>
-#include <lauxlib.h>
-#include <lualib.h>
     
 
 int main() {
 
    std::cout << "Creating game" << std::endl;
 
-   int *xpos, *ypos, *width, *height, *fullscreen;
+   int xpos, ypos, width, height, fullscreen;
 
    // read Lua script to determine initial window size infomation
-   lua_State *L = lua_open();
+   lua_State *L = luaL_newstate();
    luaopen_base(L);
    luaopen_io(L);
    luaopen_string(L);
@@ -27,14 +24,14 @@ int main() {
    lua_getglobal(L, "height");
    lua_getglobal(L, "fullscreen");
 
-   *xpos = static_cast<int>(lua_tonumber(L, -5));
-   *ypos = static_cast<int>(lua_tonumber(L, -4));
-   *width = static_cast<int>(lua_tonumber(L, -3));
-   *height = static_cast<int>(lua_tonumber(L, -2));
-   *fullscreen = lua_toboolean(L, -1);
+   xpos = static_cast<int>(lua_tonumber(L, -5));
+   ypos = static_cast<int>(lua_tonumber(L, -4));
+   width = static_cast<int>(lua_tonumber(L, -3));
+   height = static_cast<int>(lua_tonumber(L, -2));
+   fullscreen = lua_toboolean(L, -1);
 
    lua_close(L);
-   std::cout << "Xpos: " << &xpos << "\n";
+   std::cout << "Xpos: " << xpos << "\n";
    auto game = std::make_unique<Game>("1st Game", xpos, ypos, width, height, fullscreen);
 
    std::cout << "Starting game loop" << std::endl;
