19b93934b6dda68a26b9ff0af5345212ff40a934
Ryan Raettig <raettig@email.arizona.edu>
2020-01-30 14:23:43 -0500

    called update lua function from c++

--- a2/game_config/config.lua
+++ a2/game_config/config.lua
@@ -3,8 +3,8 @@
 -- read this script on program startup to specify initial
 -- window location and dimensions
 -- 
-xpos = 500
-ypos = 500
+xpos = 0
+ypos = 0
 width = 100
 height = 100
 fullscreen = false
