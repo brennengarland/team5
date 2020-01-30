a19e320cbe7038a1037f3c6c7c574b0625351fbb
Ryan Raettig <raettig@email.arizona.edu>
2020-01-30 13:36:50 -0500

    called update lua function from c++

--- a2/game_logic/Game.cpp
+++ a2/game_logic/Game.cpp
@@ -52,7 +52,7 @@ void Game::update()
    sol::state lua;
 	lua.script("import logic.lua");
 	sol::function woof = lua["update"];
-	int incrementor = update();
+	int incrementor = update(void);
    std::cout << incrementor;
 }
 
