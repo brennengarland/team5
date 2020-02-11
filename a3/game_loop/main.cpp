dac7d9d5812f02e6dac166629e6ba626bb0691c9
Brennen Linux-VM <bgarland015@gmail.com>
2020-02-11 14:35:34 -0500

    frame rate should be working

--- a3/game_loop/main.cpp
+++ a3/game_loop/main.cpp
@@ -12,21 +12,29 @@ int main() {
    auto game = std::make_unique<Game>("1st Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
 
    std::cout << "Starting game loop" << std::endl;
-   auto frame_rate = std::chrono::microseconds(16667);
+
+   // 60 frames per second = 16666666 ns for each frame
+   auto frame_rate = std::chrono::nanoseconds(16666666);
+
    while (game->running()) {
-      // 60 frames per second = 16.7ms for each frame
+      // returns our beginning timepoint to compare against
       auto t0 = std::chrono::high_resolution_clock::now();
 
       game->handle_events();
       game->update();
       game->render();
-
+   
       auto t1 = std::chrono::high_resolution_clock::now();
-      auto duration = t1 - t0;
-      auto sleep_time = std::chrono::duration_cast<std::chrono::seconds>(frame_rate - duration);
-      std::this_thread::sleep_for(sleep_time);
-      std::cout << "Loop Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count() << "\n";
-      std::cout << "Sleep time: " << sleep_time.count() << "\n\n";
+      auto elapsed_time = t1 - t0;
+      // Frame_rate - elapsed time will give us how much time is left to sleep 
+      auto sleep_time = frame_rate - elapsed_time;
+      // Sleep for however much time is left 
+      std::this_thread::sleep_for(std::chrono::duration_cast<std::chrono::seconds>(sleep_time));
+
+      // Output to verify our frame rate
+      // std::cout << "Loop Time: " << std::chrono::duration_cast<std::chrono::microseconds>(duration).count() << "\n";
+      // std::cout << "Sleep time: " << std::chrono::duration_cast<std::chrono::microseconds>(sleep_time).count() << "\n";
+      // std::cout << "Total execution time: " << std::chrono::duration_cast<std::chrono::microseconds>(duration + sleep_time).count() << "\n\n";
    }
 
    return 0;
