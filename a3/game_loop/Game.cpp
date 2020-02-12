7976c10e0c951c04aba701b9f03606e3e041cc9e
Ryan Raettig <raettig@email.arizona.edu>
2020-02-12 17:22:19 -0500

    1

--- a3/game_loop/Game.cpp
+++ a3/game_loop/Game.cpp
@@ -11,13 +11,9 @@ Game::Game(const char* title, int xpos, int ypos, int width, int height, bool fu
       flags = SDL_WINDOW_FULLSCREEN;
    }
    
-   //uncomment the following to test
-   //SDL_DestroyRenderer(renderer);
-   //SDL_DestroyWindow(window);
-   //SDL_Quit();
-   //throw std::runtime_error("Constructor Failed");
+   //uncomment the following to test SDL initilization, window constructor, or renderer constructor to test
 
-   if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+   if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
       std::cout << "Subsystems initialized..." << std::endl;
       window = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
       if (window) {
