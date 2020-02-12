fbe09adc316c867ad4da7253495b820bb2a08bfc
Ryan Raettig <raettig@email.arizona.edu>
2020-02-12 17:46:35 -0500

    1

---
+++ a3/team5/game_loop/Game.cpp
@@ -0,0 +1,85 @@
+
+#include "Game.hpp"
+#include <iostream>
+#include <stdexcept>
+
+
+Game::Game(const char* title, int xpos, int ypos, int width, int height, bool fullscreen)
+{
+   Uint32 flags{};
+   if (fullscreen) {
+      flags = SDL_WINDOW_FULLSCREEN;
+   }
+   
+   //uncomment the following to test SDL initilization, window constructor, or renderer constructor to test
+
+   if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
+      std::cout << "Subsystems initialized..." << std::endl;
+      window = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
+      if (window) {
+         std::cout << "Window created..." << std::endl;
+      }
+      else{//if window wasn't created, destroy, quit, and throw exception... destroying all items just to be sure
+         is_running = false;
+         SDL_DestroyRenderer(renderer);
+	      SDL_DestroyWindow(window);
+         SDL_Quit();
+         throw std::runtime_error("Window Constructor Failed");
+      }
+      renderer = SDL_CreateRenderer(window, -1, 0);
+      if (renderer) {
+         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+         std::cout << "Renderer created..." << std::endl;
+      }
+      else{//if renderer wasn't created, destroy, quit, and throw exception... destroying all items just to be sure
+         is_running = false;
+         SDL_DestroyRenderer(renderer);
+	      SDL_DestroyWindow(window);
+         SDL_Quit();
+         throw std::runtime_error("Renderer Constructor Failed");
+      }
+   is_running = true;
+   } else {//if SDL wasn't initialized, destroy, quit, and throw exception... destroying all items just to be sure
+      is_running = false;
+      SDL_DestroyRenderer(renderer);
+	   SDL_DestroyWindow(window);
+      SDL_Quit();
+      throw std::runtime_error("SDL Initializaion Failed");
+   }
+}
+
+Game::~Game()
+{
+   SDL_DestroyRenderer(renderer);
+   SDL_DestroyWindow(window);
+   SDL_Quit();
+   std::cout << "Game cleaned..." << std::endl;
+}
+
+void Game::handle_events()
+{
+   SDL_Event event;
+   SDL_PollEvent(&event);
+   switch (event.type) {
+      case SDL_QUIT:
+         is_running = false;
+         break;
+      default:
+         break;
+   }
+}
+
+void Game::update()
+{
+   counter++;
+   std::cout << counter << std::endl;
+}
+
+void Game::render()
+{
+   SDL_RenderClear(renderer);
+   // this is where we would add stuff to render
+   //did not add sleep here because in the pdf it said to slow main down to 60hz
+   SDL_RenderPresent(renderer);
+}
+
---
+++ a3/team5/game_loop/main.cpp
@@ -0,0 +1,53 @@
+
+#include "Game.hpp"
+
+#include <iostream>
+#include <memory>
+#include <chrono>
+#include <thread>
+#include <stdexcept>
+
+int main() {
+
+   std::cout << "Creating game" << std::endl;
+   std::unique_ptr<Game> game;
+   try
+   {
+		game = std::make_unique<Game>("1st Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+   }
+   catch(std::runtime_error x)
+   {
+	   std::cout<<"ERROR OCCURED: " << x.what() << "\n";
+	   exit(0);
+   }
+   std::cout << "Starting game loop" << std::endl;
+
+   // 60 frames per second = 16666666 ns for each frame
+   auto frame_rate = std::chrono::nanoseconds(16666666);
+
+   while (game->running()) {
+      // returns our beginning timepoint to compare against
+      auto t0 = std::chrono::high_resolution_clock::now();
+
+      game->handle_events();
+      game->update();
+      game->render();
+   
+      auto t1 = std::chrono::high_resolution_clock::now();
+      auto elapsed_time = t1 - t0;
+      // Frame_rate - elapsed time will give us how much time is left to sleep 
+      //Sleep in main(), pdf said to slow main()
+      auto sleep_time = frame_rate - elapsed_time;
+      // Sleep for however much time is left 
+      std::this_thread::sleep_for(std::chrono::duration_cast<std::chrono::seconds>(sleep_time));
+
+      //Output to verify our frame rate
+      std::cout << "Loop Time: " << std::chrono::duration_cast<std::chrono::microseconds>(elapsed_time).count() << "\n";
+      std::cout << "Sleep time: " << std::chrono::duration_cast<std::chrono::microseconds>(sleep_time).count() << "\n";
+      std::cout << "Total execution time: " << std::chrono::duration_cast<std::chrono::microseconds>(elapsed_time + sleep_time).count() << "\n\n";
+      std::cout << "Total execution time in Hertz: " << 1/((elapsed_time + sleep_time).count()/1000000000.0) << "\n\n";
+
+   }
+
+   return 0;
+}
