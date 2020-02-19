6abee7209065390e1f4c70efb2053101f3e3913e
lin2 <robert.willburn@afit.edu>
2020-02-18 19:53:24 -0500

    Tank is working

--- a4/gameobject/Game.cpp
+++ a4/gameobject/Game.cpp
@@ -1,11 +1,12 @@
 
 #include "Game.hpp"
-
+#include "Tank.cpp"
 #include <iostream>
 #include "SDL2/SDL_image.h"
 
 #include "texture_utils.hpp"
 #include "GameObject.hpp"
+
 
 SDL_Renderer* Game::renderer{};
 SDL_Window* Game::window{};
@@ -54,8 +55,8 @@ Game::~Game()
 void Game::load_level()
 {
 
-   game_objects.push_back(std::make_unique<GameObject>("../assets/images/tank-big-down.png", 0.0f, 0.0f, 0.5f, 0.5f));
-   game_objects.push_back(std::make_unique<GameObject>("../assets/images/chopper-single.png", 50.0f, 50.0f, 0.5f, 0.5f));
+   game_objects.push_back(std::make_unique<Tank>("", 0.0f, 0.0f, 0.5f, 0.5f));
+   game_objects.push_back(std::make_unique<GameObject>("../assets/images/chopper-sinngle.png", 50.0f, 50.0f, 0.5f, 0.5f));
    game_objects.push_back(std::make_unique<GameObject>("../assets/images/pacman/pacman_32x32.png", 100.0f, 100.0f, 0.5f, 0.5f));
 }
 
--- a4/gameobject/Tank.cpp
+++ a4/gameobject/Tank.cpp
@@ -3,11 +3,12 @@
 #include "Game.hpp"
 
 class Tank : public GameObject{
-	Tank::Tank(const float xpos, const float ypos, const float xvel, const float yvel)
-: xpos(xpos), ypos(ypos), xvel(xvel), yvel(yvel)
+	public:
+	Tank(const char* x, const float xpos, const float ypos, const float xvel, const float yvel)
+: GameObject("../assets/images/tank-big-down.png", xpos, ypos, xvel, yvel)
 {
-   texture = texture::load_texture("../assets/images/tank-big-down.png");
+	
 }
 	
 	
-}
+};
