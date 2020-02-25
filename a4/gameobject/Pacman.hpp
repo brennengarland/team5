6a60c0bbb93689faf5f66067ad39cb361a9d8c4e
Ryan Raettig <raettig@email.arizona.edu>
2020-02-25 14:14:38 -0500

    1

--- a4/gameobject/Pacman.hpp
+++ a4/gameobject/Pacman.hpp
@@ -8,13 +8,22 @@
 class Pacman : public GameObject{
 	public:
     //default constructor, with own image
-	Pacman(const float xpos, const float ypos, const float xvel, const float yvel): GameObject("../assets/images/pacman/pacman_32x32.png", xpos, ypos, xvel, yvel)
+	// Pacman(const float xpos, const float ypos, const float xvel, const float yvel): GameObject("../assets/images/pacman/pacman_32x32.png", xpos, ypos, xvel, yvel)
+    //     {
+    //     }
+    Pacman(const float xpos, const float ypos, const float xvel, const float yvel)
         {
+            texture = texture::load_texture("../assets/images/pacman/pacman_32x32.png");
+            xpos = xpos;
+            ypos = ypos;
+            xvel = xvel;
+            yvel = yvel;
+
         }
     //defined file constructor
-    Pacman(const char* x, const float xpos, const float ypos, const float xvel, const float yvel): GameObject(x, xpos, ypos, xvel, yvel)
-        {
-        }
+    // Pacman(const char* x, const float xpos, const float ypos, const float xvel, const float yvel): GameObject(x, xpos, ypos, xvel, yvel)
+    //     {
+    //     }
 
     ~Pacman();
     //define own behavior
