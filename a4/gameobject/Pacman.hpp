ca66df01ff5c8463ef0b51ffdb9340cd7a4c6e1d
Ryan Raettig <raettig@email.arizona.edu>
2020-02-25 14:17:37 -0500

    1

--- a4/gameobject/Pacman.hpp
+++ a4/gameobject/Pacman.hpp
@@ -11,26 +11,31 @@ class Pacman : public GameObject{
 	// Pacman(const float xpos, const float ypos, const float xvel, const float yvel): GameObject("../assets/images/pacman/pacman_32x32.png", xpos, ypos, xvel, yvel)
     //     {
     //     }
-    Pacman(const float xpos, const float ypos, const float xvel, const float yvel)
-        {
-            texture = texture::load_texture("../assets/images/pacman/pacman_32x32.png");
-            this.xpos = xpos;
-            this.ypos = ypos;
-            this.xvel = xvel;
-            this.yvel = yvel;
+        Pacman(const float xpos, const float ypos, const float xvel, const float yvel)
+             {
+                texture = texture::load_texture("../assets/images/pacman/pacman_32x32.png");
+                this.xpos = xpos;
+                this.ypos = ypos;
+                this.xvel = xvel;
+                this.yvel = yvel;
 
-        }
+            }
     //defined file constructor
     // Pacman(const char* x, const float xpos, const float ypos, const float xvel, const float yvel): GameObject(x, xpos, ypos, xvel, yvel)
     //     {
     //     }
 
-    ~Pacman();
+        ~Pacman();
     //define own behavior
-    virtual void update(const float dt) override;
-    virtual void render() override;
+        virtual void update(const float dt) override;
+        virtual void render() override;
 
     private:
+        float xpos{}, ypos{};
+        float xvel{}, yvel{};
+
+        SDL_Texture* texture{};
+        SDL_Rect src_rect, dest_rect;
 
 	
 };
